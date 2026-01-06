9144a3596c2ab8278c4669f8cd1d53c58376067e
loopbutinfite <bestrada8800@gmail.com>
2026-01-06 14:28:53 -0800

    inital commit

---
+++ sayHelloApi/Program.cs
@@ -0,0 +1,24 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+// builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+app.MapControllers();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    // app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.Run();
---
+++ sayHelloApi/controllers/SayHelloController.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Versioning;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace sayHelloApi.controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class SayHelloController : ControllerBase
+    {
+        [HttpGet]
+        [Route("Greeting/{userName}")]
+        public string Greeting(string userName)
+        {
+            return $"Hello, {userName}!";
+        }
+       
+        //CRUD Functions
+        //C reate --> POST
+        //R ead --> GET
+        //U pdate --> PUT
+        //D elete --> DELETE
+    }
+}
